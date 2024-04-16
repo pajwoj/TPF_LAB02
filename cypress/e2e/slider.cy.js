0d999102af5e244f0a1690f5f256d17a252e5add
Wojciech Pajtel <pajwoj@gmail.com>
2024-04-16 02:51:14 +0200

    do exercise

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,52 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe("ZADANIE 1 TEST", function() {
+  it("Checks if the user can traverse through slides", function() {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'London');
+    cy.get('.swiper-button-prev').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+  });
+});
+
+describe("ZADANIE 2 TEST", function() {
+  it("Checks if the slides are correct", function() {
+    cy.visit('http://localhost:3000');
+    cy.get('.card-description').should('exist');
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+    cy.get('.swiper-slide-active').should('contain', 'Italy');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.card-description').should('exist');
+    cy.get('.swiper-slide-active').should('contain', 'London');
+    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.card-description').should('exist');
+    cy.get('.swiper-slide-active').should('contain', 'Paris');
+    cy.get('.swiper-slide-active').should('contain', 'France');
+  });
+});
+
+// describe("ZADANIE 3 TEST", function() {
+//   it("Checks if the gallery is displayed correctly on different devices", function() {
+    
+//   });
+// });
+
+describe("ZADANIE 4 TEST", function() {
+  it("Checks if all elements are working correctly", function() {
+    cy.visit('http://localhost:3000');
+    cy.get(".wrapper").should("be.visible");
+    cy.get(".swiper-wrapper").children().should('have.length', 3);
+    cy.wait(2000);
+    cy.get(".swiper-button-next").should("be.visible").click();
+    cy.wait(2000);
+    cy.get(".swiper-button-prev").should("be.visible").click();
+  });
+});
